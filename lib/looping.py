cc445ff8dceb090a89963d4a64153376ee9aa217
RumiYo <yokoyamarumi@gmail.com>
2024-02-18 15:57:53 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_integers = [ integer * integer for integer in int_list ]
+    return squared_integers
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i <= 100:   
+        if i % 15 == 0:
+             print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        else:
+            print(i)
+        i += 1    
